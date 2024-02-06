9b991b2a059178dd16f51c4a41d6574852798d2c
David Williams-Young <dbwy@lbl.gov>
2024-02-06 14:30:06 -0800

    Use ChemCache for molecule / basis generation in example driver

--- examples/driver/fastscf_driver.cpp
+++ examples/driver/fastscf_driver.cpp
@@ -17,32 +17,23 @@
 #include <iostream>
 
 #include <fastscf/fastscf.hpp>
+#include <chemcache/chemcache.hpp>
 
 int main(int argc, char** argv) {
 
     // Populate modules
     pluginplay::ModuleManager mm;
+    chemcache::load_modules(mm);
     fastscf::load_modules(mm);
 
     // Create ChemicalSystem
-    simde::type::atom h1("H", 1ul, 0.0, 0.0, 0.0, 0.0);
-    simde::type::atom h2("H", 1ul, 0.0, 0.0, 0.0, 1.0);
-    simde::type::molecule mol({h1, h2});
+    std::string mol_name = "water";
+    auto mol = mm.at("NWX Molecules").run_as<simde::MoleculeFromString>(mol_name);
     simde::type::chemical_system cs(mol);
 
     // Create BasisSet
-    std::vector<double> h_sto_alpha = {3.42525091, 0.62391373, 0.16885540};
-    std::vector<double> h_sto_coeff = {0.15432897, 0.53532814, 0.44463454};
-    simde::type::contracted_gaussian h1_cg(h_sto_coeff.begin(), h_sto_coeff.end(), h_sto_alpha.begin(), h_sto_alpha.end(), 0, 0, 0);
-    simde::type::contracted_gaussian h2_cg(h_sto_coeff.begin(), h_sto_coeff.end(), h_sto_alpha.begin(), h_sto_alpha.end(), 0, 0, 1);
-    simde::type::atomic_basis_set h1_bs("STO-3G", 1, 0.0, 0.0, 0.0);
-    simde::type::atomic_basis_set h2_bs("STO-3G", 1, 0.0, 0.0, 1.0);
-    h1_bs.add_shell(chemist::ShellType::pure, 0, h1_cg);
-    h2_bs.add_shell(chemist::ShellType::pure, 0, h2_cg);
-
-    simde::type::ao_basis_set aos;
-    aos.add_center(h1_bs);
-    aos.add_center(h2_bs);
+    std::string basis_name = "sto-3g"; // This is the only supported basis in ChemCache
+    auto aos = mm.at(basis_name).run_as<simde::MolecularBasisSet>(mol);
 
     // Run module
     auto E = mm.at("FastSCF Energy").run_as<simde::AOEnergy>(aos, cs);
