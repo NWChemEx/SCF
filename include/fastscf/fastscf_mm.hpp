525696dfd665b903cd61175fda609ae8f0c5b722
Jonathan M. Waldrop <relik107@gmail.com>
2024-06-18 14:44:58 -0500

    Clean CMake; Update for PluginPlay changes

--- include/fastscf/fastscf_mm.hpp
+++ include/fastscf/fastscf_mm.hpp
@@ -15,7 +15,7 @@
  */
 
 #pragma once
-#include <pluginplay/module_manager.hpp>
+#include <pluginplay/pluginplay.hpp>
 
 namespace fastscf {
 /** @brief Loads the modules contained in the FastSCF module collection into the
