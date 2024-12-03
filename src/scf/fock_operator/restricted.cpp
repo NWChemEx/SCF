f26d9b676a654a96d84d1f9ee8fb4ae30dcfa46b
Ryan M. Richard <rrichard@ameslab.gov>
2024-12-03 13:20:31 -0600

    adds restricted fock operator builder

--- src/scf/fock_operator/restricted.cpp
+++ src/scf/fock_operator/restricted.cpp
@@ -12,32 +12,43 @@ public:
     using T_e_term  = simde::type::T_e_type;
     using V_en_term = simde::type::V_en_type;
     using V_ee_term = simde::type::V_ee_type;
-    using V_en_term = simde::type::V_en_type;
+    using V_nn_term = simde::type::V_nn_type;
 
-    RestrictedVisitor(simde::type::Fock& F, DensityType& rho) :
-      m_F_(&F), m_rho_(&rho) {}
+    RestrictedVisitor(simde::type::fock& F, const DensityType& rho) :
+      m_pF_(&F), m_prho_(&rho) {}
 
     void run(const T_e_term& T_e) { m_pF_->emplace_back(1.0, T_e.clone()); }
 
     void run(const V_en_term& V_en) { m_pF_->emplace_back(1.0, V_en.clone()); }
 
     void run(const V_ee_term& V_ee) {
-        auto es = V_ee.lhs();
-        m_pF_->emplace_back(2.0, std::make_unique<J_type>(es, *m_rho_));
-        m_pF_->emplace_back(-1.0, std::make_unique<K_type>(es, *m_rho_));
+        auto es = V_ee.at<0>();
+        m_pF_->emplace_back(2.0, std::make_unique<J_type>(es, *m_prho_));
+        m_pF_->emplace_back(-1.0, std::make_unique<K_type>(es, *m_prho_));
     }
-
-    void run(const V_nn_term& V_nn) { m_pF_->emplace_back(1.0, V_nn.clone()); }
 
 private:
     simde::type::fock* m_pF_;
-    DensityType* m_rho_;
+    const DensityType* m_prho_;
 };
+
+const auto desc = R"(
+Restricted Fock Operator Builder
+--------------------------------
+
+This module builds the Fock operator given a restricted density. Under this 
+assumption, the Fock operator is created by mapping each term in the 
+electronic Hamiltonian to itself with the exception of the electron-electron
+repulsion. The electron-electron repulsion is mapped to 2J-K where J is the
+Coulomb operator for the electrons interacting with a density and K is the 
+exchange operator for the same density.
+)";
 
 template<typename DensityType>
 TEMPLATED_MODULE_CTOR(Restricted, DensityType) {
     using pt = simde::FockOperator<DensityType>;
     satisfies_property_type<pt>();
+    description(desc);
 }
 
 template<typename DensityType>
@@ -46,10 +57,11 @@ TEMPLATED_MODULE_RUN(Restricted, DensityType) {
     using simde::type::many_electrons;
 
     const auto& [H, rho] = pt::unwrap_inputs(inputs);
+    auto H_elec          = H.electronic_hamiltonian();
 
     simde::type::fock F;
     RestrictedVisitor<DensityType> visitor(F, rho);
-    H.visit(visitor);
+    H_elec.visit(visitor);
 
     auto rv = results();
     return pt::wrap_results(rv, F);
--- tests/cxx/unit_tests/fock_operator/restricted.cpp
+++ tests/cxx/unit_tests/fock_operator/restricted.cpp
@@ -1,3 +1,4 @@
+#include "../../test_scf.hpp"
 #include <scf/scf.hpp>
 #include <simde/simde.hpp>
 
@@ -5,8 +6,17 @@ TEST_CASE("Restricted") {
     pluginplay::ModuleManager mm;
     scf::load_modules(mm);
 
-    using density_type = simde::type::e_density;
+    using density_type = simde::type::decomposable_e_density;
     using pt           = simde::FockOperator<density_type>;
 
-    auto& mod = mm.at("AO Restricted Fock Op");
+    auto& mod = mm.at("Restricted Fock Op");
+
+    SECTION("H2 Molecule") {
+        auto H   = test_scf::h2_hamiltonian();
+        auto rho = test_scf::h2_density();
+
+        auto F      = mod.run_as<pt>(H, rho);
+        auto F_corr = test_scf::h2_fock();
+        REQUIRE(F == F_corr);
+    }
 }
